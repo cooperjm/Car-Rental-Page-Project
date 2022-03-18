90408a55123b9328fcfb30af1d4523e7337c8f7a
cooperjm <cooper.jm@outlook.com>
2022-03-18 16:46:57 -0400

    Update: Vehicle Cards
    Fleshing out the look of the vehicle cards

--- src/components/vehicleCard/vehicleCard.js
+++ src/components/vehicleCard/vehicleCard.js
@@ -7,8 +7,35 @@ function vehicleCard(props) {
 		<div className={styles.card}>
 			<div className={styles.cardImages}>
 				<div className={styles.imgContainer}>
-					<img src={vehicle.exteriorImage} alt='' />
-					<img src={vehicle.interiorImage} alt='' />
+					<img src={vehicle.exteriorImage} alt={vehicle.name} />
+					<img src={vehicle.interiorImage} alt={vehicle.name + ' Interior'} />
+				</div>
+			</div>
+			<div className={styles.info}>
+				<p className={styles.year}>2022</p>
+				<p className={styles.model}>{vehicle.name}</p>
+				<div className={styles.priceContainer}>
+					<div className={styles.week}>
+						<p>
+							${vehicle.weekPrice}
+							<br></br>
+							<span className={styles.priceText}>a week</span>
+						</p>
+					</div>					
+					<div className={styles.day}>
+						<p>
+							${vehicle.dayPrice}
+							<br></br>
+							<span className={styles.priceText}>a day</span>
+						</p>
+					</div>
+				</div>
+				<div className={styles.infoList}>
+					<ul>
+						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+						<li>Lorem ipsum dolor sit.</li>
+						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, sint!</li>
+					</ul>
 				</div>
 			</div>
 		</div>
