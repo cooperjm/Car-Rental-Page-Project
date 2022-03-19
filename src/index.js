2918d251943002176ac86697d681672ad3e4a27a
cooperjm <cooper.jm@outlook.com>
2022-03-18 23:09:59 -0400

    Fix: Dropshadow
    Stop dropshadow from going too dark in dark mode

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'open-props/style';
 import 'normalize.css';
+import 'open-props/style';
 // import 'open-props/normalize';
 
 ReactDOM.render(
